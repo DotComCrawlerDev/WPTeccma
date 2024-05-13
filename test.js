7a7e4787a81da2a012cad9e873ab56fb11a6a5ec
unknown <141814450+DotComCrawlerDev@users.noreply.github.com>
2024-05-13 14:31:21 -0600

    Cambio Header Nosotros y Contacto

--- test.js
+++ test.js
@@ -1,17 +1,45 @@
-window.addEventListener('scroll', function() {
-    var header = document.getElementById('header');
-    var logo = document.getElementById('logo');
-    var navLinks = document.getElementById('nav-links');
-    var scrollTop = window.scrollY;
+// Espera a que el documento se cargue completamente
+document.addEventListener('DOMContentLoaded', function() {
+    // Obtén la URL de la página actual
+    var currentPage = window.location.href.split('/').pop();
 
-    if (scrollTop > 0) {
+    // Verifica si la página actual es Test.html
+    if (currentPage !== 'Test.html') {
+        // En todas las demás páginas, el header aparece en el segundo estado
+        var header = document.getElementById('header');
+        var navLinks = document.getElementById('nav-links');
+
         header.classList.add('scrolled');
         navLinks.style.transition = 'right 0.5s cubic-bezier(0.4, 0, 0.2, 1)'; // Transición suave con cubic-bezier
-        navLinks.style.right = '120px'; // Ajustar la posición del nav bar cuando se hace scroll
-    } else {
-        header.classList.remove('scrolled');
-        navLinks.style.transition = 'none'; // Eliminar transición cuando el usuario está en la parte superior
-        navLinks.style.right = '20px'; // Restaurar la posición original del nav bar
+        navLinks.style.right = '120px'; // Ajustar la posición del nav bar
+    }
+
+    // Espera 5 segundos antes de mostrar el recuadro de WhatsApp
+    setTimeout(function() {
+        var whatsappBurbuja = document.querySelector('.whatsapp-burbuja');
+        whatsappBurbuja.style.opacity = '1'; // Cambia la opacidad para activar la transición
+    }, 5000); // 5000 milisegundos = 5 segundos
+});
+
+window.addEventListener('scroll', function() {
+    // Obtén la URL de la página actual
+    var currentPage = window.location.href.split('/').pop();
+
+    // Verifica si la página actual es Test.html
+    if (currentPage === 'Test.html') {
+        var header = document.getElementById('header');
+        var navLinks = document.getElementById('nav-links');
+        var scrollTop = window.scrollY;
+
+        if (scrollTop > 0) {
+            header.classList.add('scrolled');
+            navLinks.style.transition = 'right 0.5s cubic-bezier(0.4, 0, 0.2, 1)'; // Transición suave con cubic-bezier
+            navLinks.style.right = '120px'; // Ajustar la posición del nav bar cuando se hace scroll
+        } else {
+            header.classList.remove('scrolled');
+            navLinks.style.transition = 'none'; // Eliminar transición cuando el usuario está en la parte superior
+            navLinks.style.right = '20px'; // Restaurar la posición original del nav bar
+        }
     }
 });
 
